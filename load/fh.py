3872edc96e45638788461cceb9efcf532777ebde
Erik Peterson <erik.exists@gmail.com>
2013-08-27 18:27:35 -0400

    Added a few functions to give Ss directory names, NOD names, etc.

--- load/fh.py
+++ load/fh.py
@@ -2,8 +2,28 @@ import os
 
 # ----
 # Global pathing...
-ROIPATH = "/data/data2/meta_accumulate/fh/roinii"    
-METAPATH = "/data/data2/meta_accumulate/fh/fidl"
+DATAPATH = "/data/data2/meta_accumulate/fh"
+ROIPATH = os.path.join(DATAPATH, "roinii")
+METAPATH = os.path.join(DATAPATH, "fidl")
+
+
+def get_subject_dirs():
+    return ["fh09", "fh11", "fh13", "fh14", "fh15", "fh17", "fh19", 
+            "fh21", "fh23", "fh24", "fh25", "fh26", "fh27", "fh28"]
+
+
+def get_subject_paths():
+    return [os.path.join(DATAPATH, subdir) for subdir in get_subject_dirs()]    
+
+
+def get_subject_fidl_paths():
+    return [os.path.join(subpath, "fidl") for subpath in get_subject_paths()]
+
+
+def get_nod_subject_names():
+    return ["fh009", "fh011", "fh013", "fh014", "fh015", "fh017", "fh019", 
+            "fh021", "fh023", "fh024", "fh025", "fh026", "fh027", "fh028"]
+
 
 def get_roi_data_paths(roi):
     return [
@@ -75,3 +95,6 @@ def get_noise_metadata_paths():
         os.path.join(METAPATH, "trtime_fh026_noise_corr_EF.csv"),
         os.path.join(METAPATH, "trtime_fh027_noise_corr_EF.csv"),
         os.path.join(METAPATH, "trtime_fh028_noise_corr_EF.csv")]
+
+
+

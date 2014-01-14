213510db75163e234ae6b7c75a9a49e564ed1f25
Erik Peterson <erik.exists@gmail.com>
2014-01-14 12:33:23 -0500

    Dir name correction (which is probably not the issue

--- mniconvert/run_pre_allredgreen.m
+++ mniconvert/run_pre_allredgreen.m
@@ -2,7 +2,7 @@ addpath('~/src/wheelerdata/mniconvert/spm/')
 
 DATAPATH = '/data/data2/meta_accumulate/redgreen/'
 
-Ss = {'redgreen1' 'redgreen2' 'redgreen5' 'redgreen7' 'redgreen8' 'redgreen9' 'redgreen10' 'redgreen11' 'redgreen12' 'redgreen14' 'redgreen15' 'redgreen16' 'redgreen17' 'redgreen18' 'redgreen19' 'redgreen22' 'redgreen24' 'redgreen25'}
+Ss = {'/redgreen1' '/redgreen2' '/redgreen5' '/redgreen7' '/redgreen8' '/redgreen9' '/redgreen10' '/redgreen11' '/redgreen12' '/redgreen14' '/redgreen15' '/redgreen16' '/redgreen17' '/redgreen18' '/redgreen19' '/redgreen22' '/redgreen24' '/redgreen25'}
 
 for ii=1:size(Ss,2),
     datadir = [DATAPATH Ss{ii}]

0775e88c6727b7f157a7a4308eba8945ff71c028
Erik Peterson <erik.exists@gmail.com>
2014-02-11 12:40:04 -0500

    Added biasbox dat option

--- load/meta.py
+++ load/meta.py
@@ -3,7 +3,7 @@ from wheelerdata.load.clock import Clock
 from wheelerdata.load.fh import FH
 from wheelerdata.load.polygon import Polygon
 from wheelerdata.load.redgreen import Redgreen
-
+from wheelerdata.load.biasbox import Biasbox
 
 def get_data(name):
     """Return the named Wheelerdata object"""
@@ -18,6 +18,8 @@ def get_data(name):
         data = Polygon()
     elif name == 'redgreen':
         data = Redgreen()
+    elif name == "biasbox":
+        data = Biasbox()
     else:
         raise ValueError('Data not understood.  Try fh, butterfly, clock,' 
                 ' polygon, or redgreen')
